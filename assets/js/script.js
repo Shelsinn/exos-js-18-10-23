17078b14e4645d33e21ee4f67a873ceb3a5e35f4
Shelsinn <vincentfourcroy@hotmail.fr>
2023-10-18 21:05:17 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+/* vvvvvvvvvvvvvvvv Exo 1 vvvvvvvvvvvvvvvv */
+
+/* var num = 23;
+var txt = parseInt("33");
+var tmp = 0;
+var tmp = txt
+var num = num + tmp
+console.log(tmp, num) */
+
+/* vvvvvvvvvvvvvvvv Exo 1 bis vvvvvvvvvvvvvvvv */
+
+/* var nom = prompt("Quel est votre nom ?");
+var prenom = prompt("Quel est votre prénom ?");
+var age = prompt("Quel est votre âge ?");
+alert("---BIENVENUE---" + "\n" + "Votre nom est : " + nom + "\n" + "Votre prénom est : " + prenom + "\n" + "Vous avez : " + age + " ans"); */
+
+/* vvvvvvvvvvvvvvvv Exo 2 partie 1 vvvvvvvvvvvvvvvv */
+
+/* var phrase = prompt("Tapez une phrase...");
+alert("La phrase tapée est : " + " ' " + phrase + " ' " + " et elle contient " + phrase.length + " caractères !") */
+
+/* vvvvvvvvvvvvvvvv Exo 2 partie 2 vvvvvvvvvvvvvvvv */
+
+/* var texte = `Je suis en distanciel aujourd'hui`;
+var dist = texte.split(" ")
+var dist3  = dist[3]
+console.log(dist);
+console.log(dist3.toUpperCase()); */
+
+/* vvvvvvvvvvvvvvvv Exo 3 vvvvvvvvvvvvvvvv */
+
+/* var age = parseInt(prompt("Quel âge avez-vous ?"));
+if (age >= 1 && age <= 17) {
+  alert("Vous n'êtes pas encore majeur.");
+} else if (age >= 18 && age <= 49) {
+  alert("Vous êtes majeur mais pas encore senior.");
+} else if (age >= 50 && age <= 59) {
+  alert("Vous êtes senior mais pas encore retraité.");
+} else if (age >= 60 && age <= 120) {
+  alert("Vous êtes retraité, profitez de votre temps libre !");
+} */
+
+/* vvvvvvvvvvvvvvvv Exo 4 vvvvvvvvvvvvvvvv */
+
+/* var nb1 = parseInt(prompt("Tapez un nombre"));
+var nb2 = parseInt(prompt("Tapez un autre nombre"));
+if (nb1 == nb2) {
+  alert("Les nombres 3 et 3 sont égaux");
+} else if (nb1 != nb2) {
+  alert("Les nombres 5 et 8 ne sont pas égaux");
+} */
+
+/* vvvvvvvvvvvvvvvv Exo 5 + 6 vvvvvvvvvvvvvvvv */
+
+/* var note = parseInt(prompt("Saisissez votre note."));
+if (note >= 0 && note <= 59) {
+  alert("Insuffisant.");
+} else if (note >= 60 && note <= 69) {
+  alert("Suffisant.");
+} else if (note >= 70 && note <= 79) {
+  alert("Bien.");
+} else if (note >= 80 && note <= 89) {
+  alert("Très bien.");
+} else if (note >= 90 && note <= 100) {
+  alert("Excellent !");
+} */
